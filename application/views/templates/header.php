30d1e5ad2b2537935df76c356446f8530ffa2e54
Purna Peramune <purnaperamune@gmail.com>
2024-04-27 08:22:36 +0530

    Updated the header to show search bars, questions and categories when users logged in

--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -22,21 +22,25 @@ defined('BASEPATH') or exit('No direct script access allowed');
                 <a class="navbar-brand" href="<?= base_url() ?>index.php"><b>KnowledgeNest</b></a>
             </div>
             <div id="navbar" class="navbar-collapse collapse">
-
+                <?php if (isset($isLoggedIn) && $isLoggedIn) { ?>
+                    <form class="navbar-form navbar-left" action="<?= base_url() ?>index.php/question/search">
+                        <input type="text" class="form-control" name="keyword" id="keyword" placeholder="Search">
+                    </form>
+                <?php } ?>
                 <ul class="nav navbar-nav navbar-right">
                     <?php if (isset($isLoggedIn) && $isLoggedIn) { ?>
                         <li>
-                            <a href="<?= base_url() ?>index.php/auth/account">Account</a>
+                            <a href="<?= base_url() ?>index.php/authentication/profile">Account</a>
                         </li>
                         <li>
-                            <a href="<?= base_url() ?>index.php/auth/logout">Sign Out</a>
+                            <a href="<?= base_url() ?>index.php/authentication/logout">Sign Out</a>
                         </li>
                     <?php } else { ?>
                         <li>
                             <a href="<?= base_url() ?>index.php">Sign up</a>
                         </li>
                         <li>
-                            <a href="<?= base_url() ?>index.php/auth/login">Sign In</a>
+                            <a href="<?= base_url() ?>index.php/authentication/login">Sign In</a>
                         </li>
                     <?php } ?>
 
@@ -46,4 +50,19 @@ defined('BASEPATH') or exit('No direct script access allowed');
     </nav>
 
     <div class="container">
+    <div class="page-header">
+            <?php if (isset($isLoggedIn) && $isLoggedIn) { ?>
+                <div class="btn-toolbar" style="justify-content: center; display: flex">
+                    <div class="col-md-2">
+                        <a href="<?= base_url() ?>index.php/question/question" class="btn btn-success btn-md"><span class="glyphicon glyphicon-question-sign" aria-hidden="true"></span> Questions</a>
+                    </div>
+                    <div class="col-md-2">
+                        <a href="<?= base_url() ?>index.php/question/categories" class="btn btn-success btn-md"><span class="glyphicon glyphicon-comment" aria-hidden="true"></span> Categories</a>
+                    </div>
+                    <div class="col-md-2">
+                        <a href="<?= base_url() ?>index.php/question/ask" class="btn btn-success btn-md" style="margin-left: 20%"><span class="glyphicon glyphicon-hand-up" aria-hidden="true"></span> Ask Question</a>
+                    </div>
+                </div>
+            <?php } ?>
+        </div>
         
